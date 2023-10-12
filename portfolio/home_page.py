4cdcf4fe63e76aa331619162783c220165f9f9b6
hkp16721 <98339621+hkp16721@users.noreply.github.com>
2023-10-12 13:01:23 +0530

    Update home_page.py

--- portfolio/home_page.py
+++ portfolio/home_page.py
@@ -248,24 +248,18 @@ if selected == 'Home':
     #     sleep(0.5)
     progress_bar = st.progress(0)
     status_text = st.empty()
-    chart = st.line_chart(np.random.randn(10, 2))
+    chart = st.scatter_chart(np.random.randn(10, 3))
 
     for i in range(100):
-        # Update progress bar.
+        # Updating progress bar.
         progress_bar.progress(i + 1)
 
-        new_rows = np.random.randn(10, 2)
-
-        # Update status text.
+        random_rows = np.random.randn(10, 3)
         status_text.text(
             'The latest random number is: %s' % new_rows[-1, 1])
-
-        # Append data to the chart.
-        chart.add_rows(new_rows)
-
-        # Pretend we're doing some computation that takes time.
+        # Appending data to the chart.
+        chart.add_rows(random_rows)
         sleep(0.1)
 
     status_text.text('Done!')
-    chart = st.title("Hi, I am Hemant Kumar")
-    st.balloons()
+    chart = st.title("Hi, I am Hemant Kumar, an aspirin data scientist")
